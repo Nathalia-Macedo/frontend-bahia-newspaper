85c1f612e09b7d706b312f172e7d799918a0d7da
Iara Assis <iaraassisnega@gmail.com>
2024-04-22 13:38:33 -0300

    feat: deixando componente visivel apenas em desktops.

--- src/componets/sections/AsideAds/index.jsx
+++ src/componets/sections/AsideAds/index.jsx
@@ -1,10 +1,27 @@
 import styles from "./style.module.scss";
 import img from "../../../assets/imgs/publicidade-1.jpg"
+import { useEffect, useState } from "react";
 
 
 export const AsideAds = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if(window.innerWidth >= 980) {
+        setIsVisible(true);
+      }else {
+        setIsVisible(false);
+      }
+    }
+    handleResize();
+  
+    window.addEventListener("resize", handleResize);
+    return () => window.addEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <aside className={styles.asideAds}>
+    <aside className={isVisible ? styles.asideAds : styles.hidden}>
       <div>
         <img src={img} alt="" />
         <img src={img} alt="" />
