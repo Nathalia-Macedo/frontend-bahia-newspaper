4a5635943d9bfdcf2ab6e21190ca786d14c918a3
Iara Assis <iaraassisnega@gmail.com>
2024-04-30 10:15:15 -0300

    style: "Atualizacao do footer"

--- src/componets/Footer/index.jsx
+++ src/componets/Footer/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/imgs/Logo Jornal da Bahia2.png";
+import Logo from "../../assets/imgs/Logo.png";
 import styles from "./style.module.scss";
 import { MdFacebook} from "react-icons/md";
 import { FaInstagram, FaWhatsapp, FaSquareXTwitter, FaLinkedin } from "react-icons/fa6";
@@ -44,32 +44,13 @@ export const Footer = () => {
                             </Link>
                         </span>
                         <strong >
-                                <h3 className="title two">Equipe:</h3>
+                            <h3 className="title two">Equipe:</h3>
                             <Link
                                 className="title two" 
-                                to="https://www.linkedin.com/in/nathalia-de-macedo-martins-4aa693253/"
-                                target="_blank" 
-                                rel="noopener noreferrer">
-                                <FaLinkedin size={20}/>
-                                Nathalia Macedo
+                                to="/equipe" 
+                                >Ver equipe completa
                             </Link>
-                            <Link
-                                className="title two" 
-                                to="https://www.linkedin.com/in/josenilsonfarias/"
-                                target="_blank" 
-                                rel="noopener noreferrer">
-                                <FaLinkedin size={20}/>
-                                Josenilson Farias
-                            </Link>
-                            <Link
-                                className="title two" 
-                                to="https://www.linkedin.com/in/iara-reis-010a89240/"
-                                target="_blank" 
-                                rel="noopener noreferrer">
-                                <FaLinkedin size={20}/>
-                                Iara Reis
-                            </Link>
-                            </strong>
+                        </strong>
                     </section>
                 </div>
             </div>
